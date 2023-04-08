e18895e823cee81f32741892e9ea2eb15516afca
Yousif Sabah <iyousifsabah@gmail.com>
2023-04-08 14:41:14 +0300

    Update methods, add queries

--- pkg/database/models/mysql/snippets.go
+++ pkg/database/models/mysql/snippets.go
@@ -11,13 +11,49 @@ type SnippetModel struct {
 }
 
 func (m *SnippetModel) Insert(title, content, expires string) (int, error) {
-	return 0, nil
+	query := "INSERT INTO snippets (title, content, expires) VALUES (?, ?, DATE_ADD(UTC_TIMESTAMP(), INTERVAL ? DAY))"
+	result, err := m.Db.Exec(query, title, content, expires)
+	if err != nil {
+		return 0, err
+	}
+
+	id, err := result.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
+
+	return int(id), nil
 }
 
 func (m *SnippetModel) Get(id int) (*models.Snippet, error) {
-	return nil, nil
+	var snippet *models.Snippet
+	query := "SELECT title, content, expires, updated_at FROM snippets WHERE id = ?"
+
+	row := m.Db.QueryRow(query, id)
+	if err := row.Scan(&snippet.Title, &snippet.Content, &snippet.Expires, &snippet.UpdatedAt); err != nil {
+		return nil, err
+	}
+
+	return snippet, nil
 }
 
 func (m *SnippetModel) Latest() ([]*models.Snippet, error) {
-	return nil, nil
+	var snippets []*models.Snippet
+	query := "SELECT * FROM snippets LIMIT 10"
+
+	rows, err := m.Db.Query(query)
+	if err != nil {
+		return nil, err
+	}
+
+	for rows.Next() {
+		var snippet *models.Snippet
+		if err := rows.Scan(&snippet.ID, &snippet.Title, &snippet.Content, &snippet.Expires, &snippet.CreatedAt, &snippet.UpdatedAt); err != nil {
+			return nil, err
+		}
+
+		snippets = append(snippets, snippet)
+	}
+
+	return snippets, nil
 }
